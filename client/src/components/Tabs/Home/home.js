f07c48a334098e3ee7fed021af9189117bc5f603
xocaid <xocaid@gmail.com>
2022-11-04 08:03:43 -0700

    Added Google Map

--- client/src/components/Tabs/Home/home.js
+++ client/src/components/Tabs/Home/home.js
@@ -1,8 +1,58 @@
+import React, { useState} from 'react';
+
+import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
+
+//Map Containter Size
+const containerStyle = {
+  width: '500px',
+  height: '500px'
+};
+
+//Default Location for Map: Los Angeles, CA
+const center = {
+  lat: 34.052235,
+  lng: -118.243683
+};
+
 const Home = () => {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: '',
+  });
+
+// if(!isLoaded) return <div>Loading...</div>
+
+  const [map, setMap] = useState(/* @type google.maps.Map */null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
 
   return (
     <div className="home-component">
-      <h1>This is the Home page.</h1>
+      <div className='home-map'>
+        <h1>This is the Home page.</h1>
+
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+        <MarkerF 
+        position={center}
+        />
+        </GoogleMap>
+      </div>
+      <button>Like</button>
     </div>
   )
 };
